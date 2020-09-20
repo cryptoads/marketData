73ba3031469b4cef7a7a635e215f4a216edf36aa
Chris Michels <chrisjohnmichels@gmail.com>
2020-09-20 16:10:50 -0400

    added ledgerx data

--- index.js
+++ index.js
@@ -46,6 +46,23 @@ function makeRequest(url) {
 
 
 //https://s3.amazonaws.com/data.ledgerx.com/json/2020-09-19.json
+async function getLedgerTotal() {
+    let url = "https://s3.amazonaws.com/data.ledgerx.com/json/2020-09-19.json";
+    let data = await makeRequest(url);
+    let total = 0;
+    let optTotal = 0;
+    data.report_data.forEach(quote => {
+    	if(quote.contract_type == "future_contract"){
+        total += quote.volume
+    	}else if(quote.contract_type == "options_contract"){
+			optTotal += quote.volume
+		}
+    });
+    console.log("LedgerX: ", total);
+    return [total, optTotal];
+}
+
+
 async function getBakktTotal() {
     let url = "https://www.theice.com/marketdata/DelayedMarkets.shtml?getContractsAsJson=&productId=23808&hubId=26066";
     let data = await makeRequest(url);
@@ -54,6 +71,17 @@ async function getBakktTotal() {
         total += quote.volume
     });
     console.log("Bakkt: ", total);
+    return (total);
+}
+
+async function getBakktCashTotal() {
+    let url = "https://www.theice.com/marketdata/DelayedMarkets.shtml?getContractsAsJson=&productId=24348&hubId=26326";
+    let data = await makeRequest(url);
+    let total = 0;
+    data.forEach(quote => {
+        total += quote.volume
+    });
+    console.log("Bakkt Cash: ", total);
     return (total);
 }
 
@@ -69,17 +97,23 @@ async function getCMETotal() {
     return (total);
 }
 
+
 async function main() {
     let ct = await getCMETotal();
     let bt = await getBakktTotal();
     ct *= 5; //adjust for 5 BTC lot size
     let msp = bt / (ct + bt) * 100;
+    let lt = await getLedgerTotal();
+    let bct = await getBakktCashTotal();
+    console.log (lt);
     console.log(typeof ct);
     console.log(typeof bt);
     console.log(msp);
     console.log(msp.toFixed(2));
-    return { bt, ct, msp, mspRounded: msp.toFixed(2) }
+    return { bt, bct, ct, msp, mspRounded: msp.toFixed(2), lt: lt[0], lot: lt[1] }
 }
+
+
 
 exports.handler = async (event) => {
 
